3401e4f98bde58d4c8e2f62d616b10fd34f3503a
Shaoqian-dot <shaoqian.huang@unsw.edu.au>
2026-05-27 22:48:12 +1000

    params for Katana full simulations

--- scripts/generate_params.R
+++ scripts/generate_params.R
@@ -5,13 +5,13 @@ mat_p_sim <- cbind(rep(c(2, 4, 8, 16), c(1, 2, 5, 10)),
                    rep(c(100, 50, 20, 10), c(1, 2, 5, 10)))
 colnames(mat_p_sim) <- c('p', 'nsim')
 params <- expand.grid(
-  family = c("gaussian"),
+  family = c("gaussian", "poisson"),
   signal = c("v1", "vp"),
   row_id = seq_len(nrow(mat_p_sim)),
   r = 80,
-  c_val = c(0.3, 0.6),
+  c_val = c(0.3, 0.6, 0.9),
   sigma2 = 1,
-  globalTest = TRUE,
+  globalTest = c(TRUE, FALSE),
   stringsAsFactors = FALSE
 )
 
