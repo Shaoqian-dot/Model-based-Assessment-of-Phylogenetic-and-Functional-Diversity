49efc2531aa02daa5f47b60b3eabef35036cc50e
Shaoqian-dot <shaoqian.huang@unsw.edu.au>
2026-06-10 22:34:54 +1000

    Update for Katana trail2 and solve J issue

--- exploration/Example_dis_sim.R
+++ exploration/Example_dis_sim.R
@@ -1,8 +1,21 @@
+p <- 5
 D <- matrix(c(0, 1, 2, 4, 4,
               1, 0, 2, 4, 4,
               2, 2, 0, 4, 4,
               4, 4, 4, 0, 2,
               4, 4, 4, 2, 0), 5, 5)
 
+# An real example distance matrix
+
+
 S <- 1 - D/5
-eigen(S)
+eigen <- eigen(S)
+V <- eigen$vectors
+J <- diag(rep(1, p)) - 1/p * matrix(1, p, p)
+V_sub <- J %*% V
+qr(V_sub)$rank
+
+S_new <- J %*% S %*% J
+eigen_new <- eigen(S_new)
+V_new <- eigen_new$vectors
+
--- scripts/generate_params.R
+++ scripts/generate_params.R
@@ -3,14 +3,14 @@ library(here)
 
 # mat_p_sim <- cbind(rep(c(5, 10, 20, 40, 80), c(1, 2, 4, 10, 20)),
 #                    rep(c(100, 50, 25, 10, 5), c(1, 2, 4, 10, 20)))
-mat_p_sim <- cbind(rep(c(5, 10, 20, 40, 80), c(1, 2, 4, 10, 20)),
-                   rep(c(20, 10, 5, 2, 1), c(1, 2, 4, 10, 20)))
+mat_p_sim <- cbind(rep(c(5), c(1)),
+                   rep(c(20), c(1)))
 colnames(mat_p_sim) <- c('p', 'nsim')
 params <- expand.grid(
   family = c("poisson"),
   signal =  NA,
   row_id = seq_len(nrow(mat_p_sim)),
-  r = c(4, 8, 16, 32, 64),
+  r = c(64),
   c_val =  NA_real_,
   sigma2 = 1,
   globalTest = c(FALSE),
