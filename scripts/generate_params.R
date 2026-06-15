cb8bf1310a532d3a0cbeb4c1829d03aa45fd5aad
Shaoqian-dot <shaoqian.huang@unsw.edu.au>
2026-06-15 22:31:46 +1000

    config change

--- scripts/generate_params.R
+++ scripts/generate_params.R
@@ -3,8 +3,8 @@ library(here)
 
 # mat_p_sim <- cbind(rep(c(5, 10, 20, 40, 80), c(1, 2, 4, 10, 20)),
 #                    rep(c(100, 50, 25, 10, 5), c(1, 2, 4, 10, 20)))
-mat_p_sim <- cbind(rep(c(5), c(1)),
-                   rep(c(20), c(1)))
+mat_p_sim <- cbind(rep(c(5), c(20)),
+                   rep(c(1), c(20)))
 colnames(mat_p_sim) <- c('p', 'nsim')
 params <- expand.grid(
   family = c("poisson"),
@@ -36,3 +36,4 @@ write.csv(params,
           here("config", "params.csv"),
           row.names = FALSE,
           quote = FALSE)
+
